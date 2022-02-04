e0a6d875413e68c2e0ce41894ca0683089a78eeb
Álamo Vinícius <alamo.cma22@gmail.com>
2022-02-04 16:12:12 -0300

    Home create

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,16 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Seja bem-vindo ao <span>Budget</span> </h1>
+            <p>Crie seus projetos e controle seu orçamento e gastos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar projeto" />
+            <img src={savings} alt="Budget" />
+        </section>
+    )
 }
 
 export default Home

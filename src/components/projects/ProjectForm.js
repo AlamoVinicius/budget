745bf8d039b7b4792d4f5bf4472b9de4b1a93672
Álamo Vinícius <alamo.cma22@gmail.com>
2022-02-10 17:17:54 -0300

    inicio da implementação da  api

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
+
 import styles from "./ProjectForm.module.css";
 
 function ProjectForm(props) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    //useeffect faz com o que o react não envie milhares de requests para o db
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -18,8 +38,12 @@ function ProjectForm(props) {
         name="budget"
         placeholder="insira o Valor total"
       />
-      <Select name="category_id" text="selecione a categoria" />
-      <SubmitButton text={props.btnText}/>
+      <Select
+        name="category_id"
+        text="selecione a categoria"
+        options={categories}
+      />
+      <SubmitButton text={props.btnText} />
     </form>
   );
 }

3d7a5dc34a4ffe74b35eca3385b812d4fe6a117e
Álamo Vinícius <alamo.cma22@gmail.com>
2022-02-12 16:12:10 -0300

    methodo 'GET' implementado project page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,35 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
+//structure
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
-
+import ProjectCard from "../projects/ProjectCard";
+//style
 import styles from "./Projects.module.css";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      Headers: { "content-type": "application/json" },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={styles.project_container}>
@@ -22,7 +40,16 @@ function Projects() {
 
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
-        <p>projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              name={project.name}
+              id={project.id}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
       </Container>
     </div>
   );

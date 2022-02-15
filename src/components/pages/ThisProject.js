843d453f6e953cc59e52eeff3d0127ee4f00effb
Álamo Vinícius <alamo.cma22@gmail.com>
2022-02-15 17:03:39 -0300

    Edit projects add

--- src/components/pages/ThisProject.js
+++ src/components/pages/ThisProject.js
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from 'uuid'
+
 import styles from "./ThisProject.module.css";
 
 import { useParams } from "react-router-dom"; // Usando parâmetros através da url
@@ -5,12 +7,20 @@ import { useState, useEffect } from "react";
 
 import Loading from "../layout/Loading";
 import Container from "../layout/Container";
+import ProjectForm from "../projects/ProjectForm";
+import Message from '../layout/Message'
+import ServiceForm from '../services/ServiceForm'
+import ServiceCard from '../services/ServiceCard'
 
 function ThisProject() {
   const { id } = useParams();
 
   const [project, setProject] = useState([]);
+  const [services, setServices] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
+  const [showServiceForm, setShowServiceForm] = useState(false);
+  const [message, setMessage] = useState();
+  const [type, setType] = useState();
 
   useEffect(() => {
     setTimeout(() => {
@@ -23,20 +33,90 @@ function ThisProject() {
         .then((resp) => resp.json())
         .then((data) => {
           setProject(data);
+          setServices(data.services)
         })
         .catch((err) => console.log(err));
     }, 300);
   }, [id]);
 
+  function editPost(project) {
+    setMessage('')
+    // budget validation
+    if (project.budget < project.cost) {
+      setMessage('O orçamento não pode ser menor que o custo do projeto!')
+      setType('error')
+      return false  //irá impedir que a função continue a executar a edição
+    }
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(data);
+        setShowProjectForm(false);
+        setMessage('Projeto atualizado com sucesso!')
+        setType('success')
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function createService(project) {
+    setMessage('')
+    // last service
+    const lastService = project.services[project.services.length - 1]
+
+    lastService.id = uuidv4()
+
+    const lastServiceCost = lastService.cost
+
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+    // maximum value validation
+    if (newCost > parseFloat(project.budget)) {
+      setMessage('Orçamento ultrapassado, verifique o valor do serviço')
+      setType('error')
+      project.services.pop()
+      return false
+    }
+
+    //add service cost to project total cost
+    project.cost = newCost
+
+    //update project
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    }).then((resp) => resp.json())
+      .then((data) => {
+        setShowServiceForm(false)
+      })
+      .catch(err => console.log(err))
+  }
+
+  function removeService () {}
+
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
   }
+
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
+  }
+
 
   return (
     <>
       {project.name ? (
         <div className={styles.project_details}>
           <Container customClass="column">
+            {message && <Message type={type} msg={message} />}
             <div className={styles.details_container}>
               <h1>Projeto: {project.name}</h1>
               <button onClick={toggleProjectForm} className={styles.btn}>
@@ -46,7 +126,6 @@ function ThisProject() {
                 <div className={styles.project_info}>
                   <p>
                     <span>Categoria:</span> {project.category.name}
-                    
                   </p>
                   <p>
                     <span>Total de orçamento:</span> R${project.budget}
@@ -57,10 +136,44 @@ function ThisProject() {
                 </div>
               ) : (
                 <div className={styles.project_info}>
-                  <p>detalhes do projeto</p>
+                  <ProjectForm
+                    handleSubmit={editPost}
+                    btnText="Concluir edição"
+                    projectData={project}
+                  />
                 </div>
               )}
             </div>
+            {/* costs and services area*/}
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço e seu custo:</h2>
+              <button onClick={toggleServiceForm} className={styles.btn}>
+                {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+              </button>
+              <div className={styles.project_info}>
+                {showServiceForm && <ServiceForm
+                  handleSubmit={createService}
+                  btnText='Adicionar serviço'
+                  projectData={project}
+                />}
+              </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass='start'>
+              {services.length > 0 &&
+                services.map((service) => (
+                  <ServiceCard
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.id}
+                    handleRemove={removeService}
+                  />
+                ))
+              }
+              {services.length === 0 && <p>Não há serviçoes cadastrados.</p>}
+            </Container>
           </Container>
         </div>
       ) : (
